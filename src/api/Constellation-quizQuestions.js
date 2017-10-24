5c90b3051606b6a1c9ecfaaa93822461416ecf8b
Steve McNally <smcnally@gmail.com>
2017-10-24 12:01:39 -0400

    gitignore updates more Game execution updates

---
+++ src/api/Constellation-quizQuestions.js
@@ -0,0 +1,35 @@
+var quizQuestions = [
+ 	{
+    // Group 1
+   	  intro: "Each of the 6MM commercial and industrial facilities in the US spends an average of $66k/year on Energy. Each wastes an average of 30% of this Energy." , 
+   	  scoreLabel: "Energy Spend" , 
+   	  relatedURL0: "https://meanbusiness.com/shepherd/ws/constellation/Constellation-State-CoName.html" , 
+   	  relatedURLLabel0: "Get Tips Tailored to You ..." ,  
+   	  relatedURL1: "https://www.constellation.com/solutions/for-your-commercial-business.html" , 
+   	  relatedURLLabel1: "Integrated Energy Strategies for Your Business" ,
+   	  relatedURL2: "https://www.constellation.com/solutions/for-your-commercial-business/renewable_energy.html" , 
+	  relatedURLLabel2: "Renewable Solutions for your Business" ,
+	  relatedURL3: "https://www.constellation.com/solutions/for-your-commercial-business/request-a-quote.html" , 
+	  relatedURLLabel3: "More Tips Tailored To You ..." ,  
+      question: "Is your annual energy bill higher or lower than average? Pick the closest dollar amount:",
+      answers: [
+          {
+              type: "Low",
+              content: "$30k",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/07/Money-FlippingThrough50s.gif?resize=300,200"
+          },
+          {
+              type: "Average",
+              content: "$70k",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/07/Money-CashWad-HairBand.jpg?resize=300,200"
+          },
+          {
+              type: "High",
+              content: "$100k",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/07/MakingMoneyAngel-sm.gif?resize=300,200"
+          },
+      ]
+  }
+ ];
+
+export default quizQuestions;
---
+++ src/api/celebQuotes2-quizQuestions.js
@@ -0,0 +1,119 @@
+var quizQuestions = [
+  {
+   // Game-wide values
+  intro: "Celeb Quotes: Who Said It?" , 
+  scoreLabel: "Celeb Quotes result" , 
+  relatedURLLabel0: "" ,
+  relatedURL0: "" , 
+  relatedURLLabel1: "USA’s #1 Growth Industry Needs Skilled Candidates. Test your Industry Knowledge" ,
+  relatedURL1: "https://extroverting.com/2017/05/27/californias-1-growth-industry-needs-skilled-candidates/" , 
+  relatedURLLabel2: "Cannabis needs Marketing, Retail, & Business majors" ,  
+  relatedURL2: "https://extroverting.com/2017/06/04/computer-data-science-opportunities-in-california/" , 
+  relatedURLLabel3: "" , 
+  relatedURL3: "" , 
+    // Group 1
+  question: '"Marijuana is an herb and a flower. God put it here. What gives the government the right to say that God is wrong?"',
+      answers: [
+          {
+              type: "Quotifying",
+              content: "Snoop Dogg",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/400px-Snoop_Dogg_outside.jpg?crop=17,12,100,30&h=188px"
+          },
+          {
+              type: "Quotifying",
+              content: "John Lennon",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/John_Lennon-Guit-Singing-Yoko.gif?resize=225,188px&h=188px"
+          },
+          {
+              type: "Quotifying",
+              content: "Bob Marley",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Bob-Marley_3.jpg?crop=7,21,100,100&h=188px"
+          },
+          {
+              type: "Quotastic",
+              content: "Willie Nelson",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Willie_Nelson_at_Farm_Aid_2009_-_Cropped.jpg?crop=7,5,100,50&h=188px"
+          },
+      ]
+  } , 
+    {
+      // Group 1
+      question: '"The illegality of cannabis is outrageous, an impediment to full utilization of a drug which helps produce the serenity and insight, sensitivity and fellowship so desperately needed in this increasingly mad and dangerous world."',
+      answers: [
+          {
+              type: "Quotifying",
+              content: "Stephen Hawking",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/800px-Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg?resize=225,188px"
+          },
+          {
+              type: "Quotifying",
+              content: "Gore Vidal",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/549px-Gore_Vidal_2_Shankbone_2009_NYC.jpg?resize=225,188"
+          },
+          {
+              type: "Quotifying",
+              content: "Neil DeGrasse Tyson",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Neil_deGrasse_Tyson-BlackHole.gif?resize=225,188px"
+          },
+          {
+              type: "Quotastic",
+              content: "Carl Sagan",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Carl_Sagan_Planetary_Society.jpg?crop=crop=2,2,100,188px"
+          },
+      ]
+  } , 
+  {
+      // Group 1
+      question: "Give a man a joint, you get him high for a day. Teach a man how to grow hydroponic weed in his garage, and he's high for life.",
+      answers: [
+          {
+              type: "Quotifying",
+              content: "Johnny Carson",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Johnny_Carson_Woody_Allen_The_Tonight_Show_1964.jpg?resize=225,188px"
+          },
+          {
+              type: "Quotifying",
+              content: "Conan O’Brien",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/466px-Conan_OBrien_speaking_at_TBS_rally_crop.jpg?resize=225,188px"
+          },
+          {
+              type: "Quotifying",
+              content: "Jon Stewart",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Jon_Stewart-Shocked.gif?resize=225,188px"
+          },
+          {
+              type: "Quotastic",
+              content: "Bill Maher",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/447px-BillMaherSept10.jpg?crop=7,4,80,60"
+          },
+      ]
+  } , 
+  {
+    // Group 1
+      question: '"Make the most you can of the Indian Hemp seed and sow it everywhere."',
+      answers: [
+          {
+              type: "Quotifying",
+              content: "Thomas Jefferson",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Gilbert_Stuart_Thomas_Jefferson.jpg?resize=225,188px"
+          },
+          {
+              type: "Quotifying",
+              content: "Abraham Lincoln",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/482px-Abraham_Lincoln.jpg?crop=7,4,80,60"
+          },
+          {
+              type: "Quotifying",
+              content: "Teddy Roosevelt",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Teddy_roosevelt.gif?resize=225,188px"
+          },
+          {
+              type: "Quotastic",
+              content: "George Washington",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/469px-Stuart-george-washington-constable-1797.jpg?crop=18,19,80,70"
+          },
+      ]
+  }
+ ];
+ 
+export default quizQuestions;
